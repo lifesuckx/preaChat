b11fce59dac5f976f465c02dd3a9ebc25597e332
lifesuckx <joe7bussiness@gmail.com>
2023-06-25 15:47:43 +0100

    rabola

--- index.js
+++ index.js
@@ -1,29 +1,56 @@
-document.getElementById("signupForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Impede o envio padrão do formulário
-  
-    // Obter os valores dos campos do formulário
-    var name = document.getElementsByName("name")[0].value;
-    var email = document.getElementsByName("email")[0].value;
-    var password = document.getElementsByName("password")[0].value;
-  
-    // Criar um objeto com os dados a serem enviados para o servidor
-    var data = {
-      name: name,
+document.addEventListener('DOMContentLoaded', function() {
+
+document.getElementById('signupForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evita o envio do formulário
+
+    // Obtenção dos valores dos campos
+    var nome = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var senha = document.getElementById('password').value;
+
+    // Criação de um objeto para armazenar os dados
+    var registro = {
+      nome: nome,
       email: email,
-      password: password
+      senha: senha
     };
+
+    // Exemplo de exibição dos dados no console
+    console.log(registro);
+
+    // Aqui você pode adicionar código para enviar os dados para um servidor, armazená-los em um banco de dados, etc.
+    window.open('login.html');
+
+    // Limpar os campos do formulário
+    document.getElementById('registroForm').reset();
+
+
+
+    document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('loginForm').addEventListener('submit', function(event) {
+      event.preventDefault(); // Evita o envio do formulário
   
-    // Enviar os dados para o servidor usando Ajax
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "signup.php", true);
-    xhr.setRequestHeader("Content-Type", "application/json");
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        // Processar a resposta do servidor (opcional)
-        var response = JSON.parse(xhr.responseText);
-        console.log(response);
+      // Obtenção dos valores dos campos
+      var email = document.getElementById('email').value;
+      var senha = document.getElementById('password').value;
+  
+      // Obtenção dos dados do registro armazenados
+      var registroArmazenado = JSON.parse(localStorage.getItem('registro'));
+  
+      if (registroArmazenado && registroArmazenado.email === email && registroArmazenado.senha === senha) {
+        alert('Login realizado com sucesso!');
+        // Aqui você pode adicionar o redirecionamento para a página de sucesso ou executar outras ações desejadas
+      } else {
+        alert('Email ou senha incorretos. Tente novamente!');
+        // Aqui você pode adicionar o tratamento para login inválido, como exibir uma mensagem de erro
       }
-    };
-    xhr.send(JSON.stringify(data));
+  
+      // Limpar os campos do formulário
+      document.getElementById('loginForm').reset();
+    window.open('msgPanel.html');
+
+    });
   });
   
+    });
+});
